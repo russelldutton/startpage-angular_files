c1eb7ace7b6096f37406f761070bcc2c602855b9
Russ <rdutton1504@gmail.com>
2017-11-02 16:45:50 +0200

    Bookmarks are now read from a json file in assets

--- src/app/searchbars/searchbars.component.ts
+++ src/app/searchbars/searchbars.component.ts
@@ -9,15 +9,15 @@ import { SearchlogoComponent } from './searchlogo/searchlogo.component';
 export class SearchbarsComponent implements OnInit {
   logos = [
     {
-      src: '../../../assets/google_logo.png',
+      src: '/assets/google_logo.png',
       link: 'http://www.google.co.za'
     },
     {
-      src: '../../../assets/youtube.png',
+      src: '/assets/youtube.png',
       link: 'http://www.youtube.co.za'
     },
     {
-      src: '../../../assets/imdb.png',
+      src: '/assets/imdb.png',
       link: 'http://www.imdb.com'
     }
   ];
