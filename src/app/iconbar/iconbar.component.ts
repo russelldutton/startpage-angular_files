efe9417a00df64ab95a53c05e957cc0e7ec78530
Russ <rdutton1504@gmail.com>
2017-11-07 16:27:41 +0200

    Finished iconbar component with service

--- src/app/iconbar/iconbar.component.ts
+++ src/app/iconbar/iconbar.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 
+import { BookmarkService } from '../bookmark.service';
+
 @Component({
-  selector: 'app-iconbar',
+  selector: 'iconbar',
   templateUrl: './iconbar.component.html',
-  styleUrls: ['./iconbar.component.css']
+  styleUrls: ['./iconbar.component.css'],
+  providers: [ BookmarkService ]
 })
 export class IconbarComponent implements OnInit {
 
-  constructor() { }
+  icons: {}[];
+
+  constructor( private bookmark: BookmarkService ) { }
 
   ngOnInit() {
+    this.bookmark.getIconList().subscribe( data => { this.icons = data['icon-bar'] } );
   }
 
 }
