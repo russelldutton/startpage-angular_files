e149114e269e4fb04fdb375474e9f85f1e56a8e8
Russ <rdutton1504@gmail.com>
2017-11-07 15:42:06 +0200

    Created new component to list basic icons to websites' home pages (unfinished)

---
+++ src/app/iconbar/iconbar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-iconbar',
+  templateUrl: './iconbar.component.html',
+  styleUrls: ['./iconbar.component.css']
+})
+export class IconbarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
