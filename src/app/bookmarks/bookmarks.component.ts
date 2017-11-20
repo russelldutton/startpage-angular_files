8d3fb125907401952ddd0c9434f6ba61f78b8d30
Russ <rdutton1504@gmail.com>
2017-11-20 16:07:11 +0200

    Used masonry instead of bootstrap cards for layout

--- src/app/bookmarks/bookmarks.component.ts
+++ src/app/bookmarks/bookmarks.component.ts
@@ -12,11 +12,21 @@ export class BookmarksComponent implements OnInit {
   
   bookmarks: {}[];
 
+  masonryOptions = {
+    itemSelector: '.grid-item',
+    columnwidth: 250,
+    gutter: 10,
+    horizontalOrder: true,
+    percentPosition: true,
+    fitWidth: true
+  };
+
   constructor( private bookmark: BookmarkService ) { }
   
   
   ngOnInit() {
     this.bookmark.getBookmarkList().subscribe( data => { this.bookmarks = data['bookmarks'] } );
+    
   }
 
 }
